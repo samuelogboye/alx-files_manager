2b6265e6e3ade56255c0a8d40adba758e0b5370d
Samuel Ogboye <ogboyesam@gmail.com>
2024-03-07 23:51:16 +0100

    redis utils

---
+++ utils/redis.js
@@ -0,0 +1,46 @@
+import redis from "redis";
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on("error", (err) =>
+      console.error(`Redis client error: ${err}`)
+    );
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) reject(err);
+        resolve(reply);
+      });
+    });
+  }
+
+  async set(key, value, seconds) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, "EX", seconds, (err, reply) => {
+        if (err) reject(err);
+        resolve(reply);
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) reject(err);
+        resolve(reply);
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
