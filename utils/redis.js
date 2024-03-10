d1aa2bc898cb13590ad8ca28b9cae9458cf42124
Samuel Ogboye <ogboyesam@gmail.com>
2024-03-10 14:53:38 +0100

    fix: users controller

--- utils/redis.js
+++ utils/redis.js
@@ -1,35 +1,35 @@
-import { createClient } from "redis";
-import { promisify } from "util";
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
-  constructor() {
+  constructor () {
     this.client = createClient();
 
-    this.client.on("error", (err) =>
+    this.client.on('error', (err) =>
       console.error(`Redis client not connected to server: ${err}`)
     );
   }
 
-  isAlive() {
+  isAlive () {
     return this.client.connected;
   }
 
   // get value for given key from redis server
-  async get(key) {
+  async get (key) {
     const redisGet = promisify(this.client.get).bind(this.client);
     const value = await redisGet(key);
     return value;
   }
 
   // set key value pair to redis server
-  async set(key, value, time) {
+  async set (key, value, time) {
     const redisSet = promisify(this.client.set).bind(this.client);
     await redisSet(key, value);
     await this.client.expire(key, time);
   }
 
   // del key vale pair from redis server
-  async del(key) {
+  async del (key) {
     const redisDel = promisify(this.client.del).bind(this.client);
     await redisDel(key);
   }
