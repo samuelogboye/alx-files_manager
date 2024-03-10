773bc3f76f102d3bbefec6ba6d845dae07d5ac35
Samuel Ogboye <ogboyesam@gmail.com>
2024-03-10 14:35:13 +0100

    fix: users controllet

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 // contains all routes
-import { Router } from "express";
+import { Router } from 'express';
 
-import AppController from "../controllers/AppController";
-import UsersController from "../controllers/UsersController";
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
-router.get("/status", AppController.getStatus);
+router.get('/status', AppController.getStatus);
 
-router.get("/stats", AppController.getStats);
+router.get('/stats', AppController.getStats);
 
-router.post("/users", UsersController.postNew);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
