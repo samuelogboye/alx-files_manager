445c1d2386cf8545e1dd95d394068028e155db63
Samuel Ogboye <ogboyesam@gmail.com>
2024-03-10 02:01:36 +0100

    chore(users): create user profile

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 // contains all routes
-import { Router } from 'express';
+import { Router } from "express";
 
-import AppController from '../controllers/AppController';
+import AppController from "../controllers/AppController";
+import UsersController from "../controllers/UsersController";
 
 const router = Router();
 
-router.get('/status', AppController.getStatus);
+router.get("/status", AppController.getStatus);
 
-router.get('/stats', AppController.getStats);
+router.get("/stats", AppController.getStats);
+
+router.post("/users", UsersController.postNew);
 
 module.exports = router;
